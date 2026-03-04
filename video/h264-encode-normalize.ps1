a7c06b39cef2d41c8651abaa345c9447c8a9bcd7
suke3 <35905884+suke-gh@users.noreply.github.com>
2026-03-05 01:12:18 +0900

    ver.0.0.14a

--- video/h264-encode-normalize.ps1
+++ video/h264-encode-normalize.ps1
@@ -10,20 +10,21 @@ if ($null -eq $inputFile) {
 
 Write-Host "Checking the loudness... : ${inputFile}"
 $jsonFile = "loudness-${inputFile}.json"
-ffmpeg -i ${inputFile} -filter:a loudnorm=I=-14:LRA=23:TP=-1:offset=0:print_format=json -vn -f null - -hide_banner 2> $jsonFile
-Get-Content $jsonFile -Tail 12 | Tee-Object -FilePath $jsonFile
+ffmpeg -i ${inputFile} -filter:a loudnorm=I=-14:LRA=23:TP=-1:offset=0:print_format=json -vn -f null - -hide_banner 2> ${jsonFile}
+Get-Content ${jsonFile} -Tail 12 | Tee-Object -FilePath ${jsonFile}
 
 # Built values of filter option.
 $jsonData = Get-Content -Raw $jsonFile | ConvertFrom-Json
-$audioFilterContent = "loudnorm=I=-14:LRA=23:TP=-1:offset=0" `
-    + ":measured_I=$($jsonData.input_i)" `
-    + ":measured_TP=$($jsonData.input_tp)" `
-    + ":measured_LRA=$($jsonData.input_lra)" `
-    + ":measured_thresh=$($jsonData.input_thresh)" `
-    + ":offset=$($jsonData.target_offset)" `
-    + ':linear=false' `
-    + ',anlmdn=s=0.00001:p=0.002:r=0.002'
-Write-Output "filter info : ${audioFilterContent}"
+$audioFilterContent = "loudnorm=I=-14:LRA=23:TP=-1:offset=0:" `
+    + "measured_I=$($jsonData.input_i):" `
+    + "measured_TP=$($jsonData.input_tp):" `
+    + "measured_LRA=$($jsonData.input_lra):" `
+    + "measured_thresh=$($jsonData.input_thresh):" `
+    + "offset=$($jsonData.target_offset):" `
+    + 'linear=false,anlmdn=s=0.00001:p=0.002:r=0.002'
+
+$timeStamp = Get-Date -Format "yyyy/MM/dd HH:mm:ss"
+Out-File -FilePath ".\loudnessLogs.txt" -InputObject "[${timeStamp}] (${inputFile}) ${audioFilterContent}" -Append
 
 ffmpeg -i ${inputFile} `
     -codec:v copy `
